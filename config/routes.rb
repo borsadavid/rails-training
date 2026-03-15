1d5684d4fbf2e8840890dd8bd07ab46f06912fc5
Borsa-m <mihail27619@gmail.com>
2026-03-15 19:20:46 +0200

    gata tema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  resources :users
+  
   # Defines the root path route ("/")
-  root "application#index"
+  root "home#index"
 end
