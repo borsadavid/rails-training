3194e689a3c20a8fb22048dc5abed0904536b11e
Borsa-m <mihail27619@gmail.com>
2026-04-23 20:02:36 +0300

    feed, postari

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_15_165611) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_23_161737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,5 +28,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_15_165611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "birthday"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
